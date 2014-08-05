7b9a59a20a905d044f9c31a07071aa39d9c3c7ef
Yann Gouffon <yago@yago-art.ch>
2014-08-05 10:27:40 +0200

    run Toni's generator

---
+++ assets/README.md
@@ -0,0 +1,1 @@
+# Antistatique Styleguide Styleguide
---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+'use strict';
+
+/**
+ * Import plugins
+ */
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+/**
+ * Build vendors dependencies
+ */
+gulp.task('vendors', function() {
+
+  /**
+   * CSS VENDORS
+   */
+  gulp.src([
+        ''
+      ])
+      .pipe($.concat('vendors.css'))
+      .pipe($.minifyCss())
+      .pipe(gulp.dest('build/css'));
+
+  /**
+   * JS VENDORS
+   * (with jQuery and Bootstrap dependencies first)
+   */
+
+  gulp.src([
+      'bower_components/jquery/jquery.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/affix.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/alert.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/button.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/carousel.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/collapse.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/dropdown.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/modal.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tooltip.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/popover.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/scrollspy.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tab.js',
+      'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/transition.js'
+    ])
+    .pipe($.concat('vendors.min.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('build/js'));
+
+
+  /**
+   * FONTS SOURCES
+   * Important to add the bootstrap fonts to avoid issues with the fonts include path
+   */
+  gulp.src([
+      'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*',
+      'assets/fonts/*'
+    ])
+    .pipe(gulp.dest('build/fonts'));
+});
+
+/**
+ * Build styles from SCSS files
+ * With error reporting on compiling (so that there's no crash)
+ */
+gulp.task('styles', function() {
+  return gulp.src('assets/sass/antistatique.scss')
+    .pipe($.rubySass())
+      .on('error', $.util.beep)
+      .on('error', $.notify.onError(function (error) {
+        return 'Message to the notifier: ' + error.message;
+      }))
+    .pipe($.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('build/css'));
+});
+
+/**
+ * Build JS
+ * With error reporting on compiling (so that there's no crash)
+ */
+gulp.task('scripts', function() {
+  gulp.src('assets/js/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.concat('main.js'))
+    .pipe(gulp.dest('build/js'))
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe($.uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+/**
+ * Build Hologram Styleguide
+ */
+gulp.task('styleguide', $.shell.task([
+  'hologram'
+]));
+
+/**
+ * Default task
+ */
+gulp.task('default', ['vendors', 'styles', 'scripts', 'watch', 'styleguide']);
+
+/**
+ * Watch task
+ */
+gulp.task('watch', function() {
+  gulp.watch('assets/sass/**/*.scss', ['styles', 'styleguide']);
+  gulp.watch('assets/js/*.js', ['scripts', 'styleguide']);
+});
+
